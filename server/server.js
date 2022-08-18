98ab8e6c887c374b9f3442d896365a4ffae56149
David Mok <davidemok@gmail.com>
2022-08-18 13:29:02 -0500

    added rollbar :(

--- server/server.js
+++ server/server.js
@@ -6,6 +6,15 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('client'))
 
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '0847950fb90641a1a46b78f00a546599',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+rollbar.log("we are under assault")
+
 const { getHTML, getCSS, getJava } = require("./controller/controller.js")
 
 app.get('/', getHTML)
